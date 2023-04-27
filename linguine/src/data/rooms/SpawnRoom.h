e4616d74520e005e92449e0fb98ece366f8963eb
Justin Driggers <justin@justindriggers.com>
2023-04-26 23:13:05 -0400

    Formatting

--- linguine/src/data/rooms/SpawnRoom.h
+++ linguine/src/data/rooms/SpawnRoom.h
@@ -5,8 +5,8 @@
 namespace linguine {
 
 class SpawnRoom : public Room {
-public:
-  SpawnRoom() : Room(5, 5, {2, 5}, {2, -1}, {5, 2}, {-1, 2}, false) {}
+  public:
+    SpawnRoom() : Room(5, 5, {2, 5}, {2, -1}, {5, 2}, {-1, 2}, false) {}
 };
 
 }  // namespace linguine
